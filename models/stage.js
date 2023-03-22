96f96c570b05e6a30e563c5067bd5977e4306c65
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-03-21 21:29:43 -0700

    sequelize Stage

---
+++ models/stage.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Stage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Stage.init({
+    stage_ig: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    crowd_capacity: {
+      type: DataTypes.SMALLINT,
+      allowNull:false
+    },
+    food: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+  }, {
+    sequelize,
+    modelName: 'Stage',
+    tableName: 'stages',
+    timestamps: false
+  });
+  return Stage;
+};
